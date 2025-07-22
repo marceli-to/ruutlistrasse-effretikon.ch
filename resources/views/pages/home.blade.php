1bfb6a380e62a0f39b6cc4d6f2e44026a11406e4
Marcel Stadelmann <m@marceli.to>
2025-07-22 10:52:12 +0200

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
   <x-media.image 
     :image="'ruetlistrasse-effretikon-visualisierung'" 
     :alt="'Visualisierung'" />
-    <x-icons.brand class="absolute z-20 left-10 top-10 md:top-40 md:left-40 w-160 h-auto md:w-[240px] xl:left-60 xl:top-60 xl:w-[320px] 2xl:w-[400px]" />
+    <x-icons.brand class="absolute z-20 left-10 top-10 md:top-40 md:left-40 w-160 h-auto md:w-[240px] xl:left-40 xl:top-40 2xl:left-50 2xl:top-50 xl:w-[300px] 2xl:w-[400px]" />
 </x-layout.section>
 
 <x-layout.section class="bg-creme">
@@ -30,14 +30,16 @@
     @livewire('create-inquiry')
     <div class="max-w-3xl mt-24 lg:mt-32">
       <p >Wie freuen uns auf den Vermarktungsstart!<br>Ihr RED-Team</p>
-      <p>
-        <img src="/media/img/logo-red-real-estate.png" alt="Logo RED Real Estate AG" class="block mb-10 lg:mb-15 w-100 h-auto" width="581" height="667" />
-        <strong>RED Real Estate AG</strong><br>
-        Seehofstrasse 4<br>
-        8008 Zürich<br>
-        Telefon <a href="tel:0432110400" target="_blank" aria-label="Telefon Nummer" class="hover:underline underline-offset-2 decoration-1">043 211 04 00</a><br>
-        <a href="mailto:office@redrealestate.ch" target="_blank" aria-label="E-Mail Adresse" class="hover:underline underline-offset-2 decoration-1">office@redrealestate.ch</a>
-      </p>
+      <div class="flex gap-x-20">
+        <img src="/media/img/logo-red-real-estate.png" alt="Logo RED Real Estate AG" class="block mb-10 lg:mb-15 w-80 lg:w-90 h-auto" width="581" height="667" />
+        <div>
+          <strong>RED Real Estate AG</strong><br>
+          Seehofstrasse 4<br>
+          8008 Zürich<br>
+          Telefon <a href="tel:0432110400" target="_blank" aria-label="Telefon Nummer" class="hover:underline underline-offset-2 decoration-1">043 211 04 00</a><br>
+          <a href="mailto:office@redrealestate.ch" target="_blank" aria-label="E-Mail Adresse" class="hover:underline underline-offset-2 decoration-1">office@redrealestate.ch</a>
+        </div>
+      </div>
     </div>
   </x-layout.inner>
 </x-layout.section>
